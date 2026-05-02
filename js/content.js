0e43c0d853fa2c8d8df7562cbcf299278d7cb0f5
tuffchippy <alexwither988@gmail.com>
2026-05-02 19:46:46 +1200

    Update content.js

--- js/content.js
+++ js/content.js
@@ -4,7 +4,7 @@ import { round, score } from './score.js';
  * Path to directory containing `_list.json` and all levels
  * FIX: remove leading "/" for GitHub Pages
  */
-const dir = '/chippy-bag-demon-list/data'; // ✅ FIXED
+const dir = '/chippy-bag-demon-list/data'; // ✅ FIXE D
 
 /**
  * Fetch list of levels
